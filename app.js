e0bfa49d076e0be3074388e13c7e6d5d46f61d44
Kurt Reicel Nunez <kurtreicel@gmail.com>
2023-01-26 23:30:47 +0800

    added comment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // JSON PARSER
 app.use(bodyParser.json());
 
+// ROUTES
 const userRoutes = require("./routes/userRoutes");
 app.use("/api/v1/user", userRoutes);
-
 module.exports = app;
